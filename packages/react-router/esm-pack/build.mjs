334a1a786f6414e02eda9866ef5cfe17319c42b7
breeze <baskwind@163.com>
2024-12-17 14:18:24 +0800

    version: 7.0.2

---
+++ packages/react-router/esm-pack/build.mjs
@@ -0,0 +1,33 @@
+import path from "path";
+import fs from "fs";
+import esbuild from "esbuild";
+
+const license = fs.readFileSync("./LICENSE.md", "utf-8");
+
+export const buildEsm = async (inputPath, outputPath) => {
+  const input = path.resolve(import.meta.dirname, "../", inputPath);
+  const output = path.resolve(import.meta.dirname, outputPath);
+
+  await esbuild.build({
+    entryPoints: [input],
+    outfile: output,
+    bundle: true,
+    minify: true,
+    legalComments: "none",
+    sourcemap: false,
+    format: "esm",
+    target: "esnext",
+    banner: {
+      js: `/**\n${license
+        .trim()
+        .split("\n")
+        .map((e) => ` * ${e}`)
+        .join("\n")}\n */`,
+    },
+    footer: {
+      js: `/* build by esm pack */`,
+    },
+  });
+};
+
+buildEsm("index.ts", "index.mjs");
